721041d8e78931b26f804c83db1056d4abdbc423
kahenya-anita <anitakahenya1@gmail.com>
2020-09-07 23:04:35 +0300

    Add save user test

--- user_test.py
+++ user_test.py
@@ -24,6 +24,14 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.acc_type,"facebook")
         self.assertEqual(self.new_user.password,"@anita21")
         self.assertEqual(self.new_user.confirm_password,"@anita21")
+    
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user object list
+        '''
+        self.new_user.save_user() # saving the new user info
+        self.assertEqual(len(User.user_list),1)
 
 if __name__ == '__main__':
     unittest.main()
